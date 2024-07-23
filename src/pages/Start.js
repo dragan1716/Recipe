77a2f47cafb3fca92b00255f24c7a2af3e39b72e
Dragan1716 <dragan.badnjar17@gmail.com>
2024-07-23 11:20:32 +0200

    Start page component

--- src/pages/Start.js
+++ src/pages/Start.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Start.css";
 import { IoIosArrowRoundForward } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const Start = () => {
   return (
@@ -9,7 +10,12 @@ const Start = () => {
         <div className="absolute bg-gradient-to-t from-black w-full h-full"></div>
       </div>
       <div className="start-content absolute top-0 left-0 h-full w-full text-center text-white pb-[80px] pt-[80px] flex flex-col justify-between">
-        <div className="h-full">
+        <div className="h-full flex flex-col justify-center items-center">
+          <img
+            src="images/image 11.png"
+            alt="hat"
+            className="mx-auto lg:w-32 mb-6"
+          />
           <h4 className="lg:text-3xl">
             <span className="font-medium">60k+</span> Premium recipes
           </h4>
@@ -19,13 +25,15 @@ const Start = () => {
             Let's <br /> Cooking
           </h1>
           <p className="mt-10 lg:text-3xl">Find the best recipes for cooking</p>
-          <button className="custom-button rounded-xl mt-8 font-medium text-md  cursor-pointer justify-center items-center inline-flex py-3 px-6 bg-red-500 hover:bg-red-700">
-            Start cooking{" "}
-            <span className="arrow ml-2 text-3xl">
-              {" "}
-              <IoIosArrowRoundForward />
-            </span>
-          </button>
+          <Link to="/signup">
+            <button className="custom-button rounded-xl mt-8 font-medium text-md  cursor-pointer justify-center items-center inline-flex py-3 px-6 bg-red-500 hover:bg-red-700">
+              Start Cooking{" "}
+              <span className="arrow ml-2 text-3xl">
+                {" "}
+                <IoIosArrowRoundForward />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
