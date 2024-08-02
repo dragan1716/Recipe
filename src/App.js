5f0272e040a1101d861e3c965d004c4b0692781f
Dragan1716 <dragan.badnjar17@gmail.com>
2024-08-02 19:01:17 +0200

    App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import Start from "./pages/Start";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { AuthContextProvider } from "./context/AuthContext";
+import Home from "./pages/Home";
+
 function App() {
   //https://api.spoonacular.com/recipes/complexSearch
-  return <div>test app</div>;
+  return (
+    <AuthContextProvider>
+      <div>
+        <Routes>
+          <Route path="/start" element={<Start />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </AuthContextProvider>
+  );
 }
 
 export default App;
